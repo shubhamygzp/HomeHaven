7c6b21c995ce7102db373111496f2340c9c34ee4
Shubham Yadav <shubhamgzpyadav@gmail.com>
2026-04-19 22:59:08 +0530

    Typo Fixed

--- controllers/user.js
+++ controllers/user.js
@@ -14,7 +14,7 @@ module.exports.signup = async (req, res) => {
       if (err) {
         return next(err);
       }
-      req.flash("success", "Welcome to Wanderlust!");
+      req.flash("success", "Welcome to HomeHaven!");
       res.redirect("/listings");
     });
   } catch (e) {
@@ -28,7 +28,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-  req.flash("success", "Welcome back to Wanderlust!");
+  req.flash("success", "Welcome back to HomeHaven!");
   // res.redirect("/listings");
   let redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
